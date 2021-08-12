e06f999895959f9fbffa93c5f675aef7cece860e
Pedro Ojeda-May <pedroojeda2011@gmail.com>
2021-08-12 11:17:22 +0200

    LAMMPS version, readme

--- hands-ons/4.application-usage/LAMMPS/job-mpi.sh
+++ hands-ons/4.application-usage/LAMMPS/job-mpi.sh
@@ -8,10 +8,10 @@
 #SBATCH -n 28
 #SBATCH --exclusive
 
-#Load modules necessary for running LAMMPS
-module load icc/2017.1.132-GCC-6.3.0-2.27 ifort/2017.1.132-GCC-6.3.0-2.27  impi/2017.1.132
-module load LAMMPS/31Mar17
+#Purge and load modules necessary for running LAMMPS
+ml purge
+ml GCC/8.3.0  OpenMPI/3.1.4
+ml LAMMPS/3Mar2020-Python-3.7.4-kokkos 
 
 #Execute LAMMPS
-#srun lmp_intel_cpu -in step4.0_minimization.inp 
-srun lmp_intel_cpu -in step4.1_equilibration.inp
+srun lmp -in step4.1_equilibration.inp
--- hands-ons/4.application-usage/LAMMPS/job-omp.sh
+++ hands-ons/4.application-usage/LAMMPS/job-omp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -A staff
+#SBATCH -A Project_ID
 #Asking for 10 min.
 #SBATCH -t 00:10:00
 #Number of nodes
@@ -9,12 +9,11 @@
 #SBATCH -c 2
 #SBATCH --exclusive
 
-#Load modules necessary for running LAMMPS
-module load icc/2017.1.132-GCC-6.3.0-2.27 ifort/2017.1.132-GCC-6.3.0-2.27  impi/2017.1.132
-module load LAMMPS/31Mar17
-
+#Purge and load modules necessary for running LAMMPS
+ml purge
+ml GCC/8.3.0  OpenMPI/3.1.4
+ml LAMMPS/3Mar2020-Python-3.7.4-kokkos 
 export OMP_NUM_THREADS=2
 
 #Execute LAMMPS
-#srun lmp_intel_cpu -in step4.0_minimization.inp 
-srun lmp_intel_cpu -in step4.1_equilibration.inp
+srun lmp -in step4.1_equilibration.inp
