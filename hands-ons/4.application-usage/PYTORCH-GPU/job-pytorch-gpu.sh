0ad8f706124bd83a57404cd1c60d90c7ca6f30d2
Pedro Ojeda-May <pedroojeda2011@gmail.com>
2021-08-19 12:01:16 +0200

    PYTORCH-GPU versions and readme

--- hands-ons/4.application-usage/PYTORCH-GPU/job-pytorch-gpu.sh
+++ hands-ons/4.application-usage/PYTORCH-GPU/job-pytorch-gpu.sh
@@ -1,18 +1,16 @@
 #!/bin/bash 
 # Remember to change this to your own project ID! 
 #SBATCH -A Project_ID
-#SBATCH --time=00:03:00
+#SBATCH --time=00:05:00
 #SBATCH --error=job.%J.err
 #SBATCH --output=job.%J.out
 #SBATCH --gres=gpu:k80:1
 
 ml purge
-module add GCC/8.2.0-2.31.1
-module add Python/3.7.2
-module add CUDA/10.1.105
-module add OpenMPI/3.1.3
-module add PyTorch/1.2.0-Python-3.7.2
-module add matplotlib/3.0.3-Python-3.7.2
-module add torchvision/0.3.0-Python-3.7.2
+
+ml GCC/10.2.0  CUDA/11.1.1  OpenMPI/4.0.5
+ml matplotlib/3.3.3
+ml PyTorch/1.7.1 
+ml torchvision 
 
 srun python pytorch_gpu.py
