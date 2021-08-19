db4d39fdb556f0b398dd28bf817d86c5a571ca4d
Pedro Ojeda-May <pedroojeda2011@gmail.com>
2021-08-19 13:49:16 +0200

    TENSORFLOW versions readme

--- hands-ons/4.application-usage/TENSORFLOW/hello_tensorflow.py
+++ hands-ons/4.application-usage/TENSORFLOW/hello_tensorflow.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
 import tensorflow as tf
+
+tf.compat.v1.disable_eager_execution()
+
 hello = tf.constant('Hello, TensorFlow!')
-sess = tf.Session()
+sess = tf.compat.v1.Session()
 print(sess.run(hello))
--- hands-ons/4.application-usage/TENSORFLOW/loss.py
+++ hands-ons/4.application-usage/TENSORFLOW/loss.py
@@ -1,36 +1,38 @@
-####!/usr/share/python
+
 
 import tensorflow as tf
+
+tf.compat.v1.disable_eager_execution()
 
 #Parameters
 W = tf.Variable([.3],tf.float32)
 b = tf.Variable([-.3],tf.float32)
 
 #Input and output
-x = tf.placeholder(tf.float32)
+x = tf.compat.v1.placeholder(tf.float32)
 
 linear_model = W*x+b
 
-y = tf.placeholder(tf.float32)
+y = tf.compat.v1.placeholder(tf.float32)
 
 #Loss 
 square_delta = tf.square(linear_model-y)
 loss = tf.reduce_sum(square_delta)
 
 #Optimize
-optimizer = tf.train.GradientDescentOptimizer(0.01)
+optimizer = tf.compat.v1.train.GradientDescentOptimizer(0.01)
 train = optimizer.minimize(loss)
 
-init = tf.global_variables_initializer()
+init = tf.compat.v1.global_variables_initializer()
 
-sess = tf.Session()
+sess = tf.compat.v1.Session()
 
 sess.run(init)
 
 for i in range (1000):
     sess.run(train,{x:[1,2,3,4],y:[0,-1,-2,-3]})
+    print(sess.run(loss,{x:[1,2,3,4],y:[0,-1,-2,-3]}))
 
-#print(sess.run(loss,{x:[1,2,3,4],y:[0,-1,-2,-3]}))
 
 print(sess.run([W,b]))
 
