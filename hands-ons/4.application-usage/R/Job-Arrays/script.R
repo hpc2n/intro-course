ad8bdb50e3091ad90a3e119e04483c2a70e876d3
Pedro Ojeda-May <pedroojeda2011@gmail.com>
2022-08-30 13:49:14 +0200

    added one env var to the arrays R script

--- hands-ons/4.application-usage/R/Job-Arrays/script.R
+++ hands-ons/4.application-usage/R/Job-Arrays/script.R
@@ -1,5 +1,7 @@
-my_id <- Sys.getenv("SLURM_ARRAY_TASK_ID")
-cat(sprintf("This is my Array ID %s", my_id))
+job_id <- Sys.getenv("SLURM_ARRAY_JOB_ID")
+cat(sprintf("This is job ID %s", job_id))
+task_id <- Sys.getenv("SLURM_ARRAY_TASK_ID")
+cat(sprintf("This is task ID %s", task_id))
 
 Sys.sleep(30)
 my_id
