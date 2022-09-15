434584232f7b8ee33903a99bd8eac49b7c46ce8e
Birgitte Brydsö <bbrydsoe@hpc2n.umu.se>
2022-09-15 12:01:59 +0200

    Update multiple-parallel.sh

--- hands-ons/3.usage/multiple-parallel.sh
+++ hands-ons/3.usage/multiple-parallel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -A SNIC2021-22-642
+#SBATCH -A SNIC2022-22-642
 # Since the files run sequentially I only need enough cores for the largest of them to run 
 #SBATCH -n 14
 # Remember to ask for enough time for all jobs to complete
