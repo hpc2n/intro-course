b251b5708e2772c0ce49b369f7022c87f857c599
Birgitte Brydsö <bbrydsoe@hpc2n.umu.se>
2022-09-15 11:56:49 +0200

    Update mpi_greeting.sh

--- hands-ons/3.usage/mpi_greeting.sh
+++ hands-ons/3.usage/mpi_greeting.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Remember to change this to your own Project ID after the course! 
-#SBATCH -A SNIC2021-22-642
+#SBATCH -A SNIC2022-22-642
 # Number of tasks - default is 1 core per task 
 #SBATCH -n 14
 #SBATCH --time=00:05:00
