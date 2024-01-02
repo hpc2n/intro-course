817808c4df4d1df6435c27f91427b1832fd1c1d0
Pedro Ojeda-May <pedroojeda2011@gmail.com>
2024-01-02 07:21:48 +0100

    MATLAB: job arrays example

--- hands-ons/4.application-usage/MATLAB/Job-Arrays/job.sh
+++ hands-ons/4.application-usage/MATLAB/Job-Arrays/job.sh
@@ -3,6 +3,7 @@
 #Asking for 12 min.
 #SBATCH -t 00:12:00
 #SBATCH --array=1-28
+#SBATCH -c 1
 #Writing output and error files
 #SBATCH --output=Array_test.%A_%a.out
 #SBATCH --error=Array_test.%A_%a.error
@@ -11,6 +12,6 @@
 #Load modules necessary for running MATLAB
 ml MATLAB/2021b
 
-srun matlab -nodisplay -singleCompThread -r "funct($SLURM_ARRAY_TASK_ID);exit" 
+matlab -nodisplay -nojvm -r "funct($SLURM_ARRAY_TASK_ID);exit" 
 
 exit 0
