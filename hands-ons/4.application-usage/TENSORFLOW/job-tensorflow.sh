734a958b191c7499f74115cea720f8d7cabc8ccd
Pedro Ojeda-May <pedroojeda2011@gmail.com>
2024-01-19 13:58:47 +0100

    TENSORFLOW: v100 card used in batch scritp

--- hands-ons/4.application-usage/TENSORFLOW/job-tensorflow.sh
+++ hands-ons/4.application-usage/TENSORFLOW/job-tensorflow.sh
@@ -4,10 +4,10 @@
 #SBATCH -t 00:10:00
 #Number of nodes
 #SBATCH -N 1
-#SBATCH --exclusive
-#Ask for 2 GPU cards
-#For V100 cards uncomment the following line and comment the line above
-#SBATCH --gres=gpu:v100:2
+#Ask for 1 GPU cards
+#SBATCH --gres=gpu:v100:1
+# If you ask for 2 GPU cards uncomment the following line
+##SBATCH --exclusive
 
 ml GCC/10.2.0  CUDA/11.1.1  OpenMPI/4.0.5
 ml TensorFlow/2.4.1 
