67b5f8e20d58a3007dbaa4455c681124921a1a48
Pedro Ojeda-May <pedroojeda2011@gmail.com>
2024-08-26 09:34:28 +0200

    ALPHAFOLD: test case

---
+++ exercises/ALPHAFOLD/job.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH -A Project_ID      # Your project ID
+#SBATCH -J multimer        # Job name in the queue 
+#SBATCH -t 08:00:00        # Wall time 
+## lines starting with double ## are considered as comments
+# For a whole node, 2 GPU cards, (could be useful depending on the 
+# length of the sequence and number nmers
+##SBATCH -c 28
+# Using 2 v100 cards
+##SBATCH --gres=gpu:v100:2
+# Using 2 a100 cards
+##SBATCH --gres=gpu:a100:2
+##SBATCH --exclusive
+# For short sequences and monomers half node, 1 v100 GPU card, would work 
+#SBATCH -c 14 
+#SBATCH --gres=gpu:v100:1
+
+# Clean the environment from loaded modules
+ml purge > /dev/null 2>&1
+
+# Load AlphaFold
+ml GCC/11.3.0  OpenMPI/4.1.4 
+ml AlphaFold/2.3.1-CUDA-11.7.0
+
+# Comment or uncomment the following exporting variables depending on whether
+# you want to use a whole node or half or the node
+# For a whole node as suggested above
+#export ALPHAFOLD_HHBLITS_N_CPU=28
+# For a half node as suggested above
+export ALPHAFOLD_HHBLITS_N_CPU=14
+
+alphafold --fasta_paths=my_fasta_sequence.fasta --max_template_date=2024-01-10 --model_preset=monomer  --output_dir=$PWD 
