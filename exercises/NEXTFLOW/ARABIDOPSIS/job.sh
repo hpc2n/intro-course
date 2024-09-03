de8104b7728d1bcc6d9212c46bb733665f9e4893
Pedro Ojeda-May <pedroojeda2011@gmail.com>
2024-09-03 11:08:52 +0200

    NEXTFLOW: usage

---
+++ exercises/NEXTFLOW/ARABIDOPSIS/job.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -A hpc2n202Q-XYZ
+#SBATCH -N 1
+#SBATCH -n 14
+#SBATCH --output=Chip_seq_analysis.out
+#SBATCH --error=Chip_seq_analysis.err
+#SBATCH --time=02:50:00
+
+ml Nextflow/24.04.2
+
+#Variable for Java
+export NXF_OPTS='-Xms1g -Xmx4g'
+#Cache dir for singularity image storage
+export NXF_SINGULARITY_CACHEDIR=$PWD/sing-img
+#Home folder for Nextflow
+export NXF_HOME=$PWD/home-nextflow
+
+nextflow run nf-core/rnaseq \
+    -profile singularity \
+    --input design_test.csv \
+    --genome 'TAIR10' \
+    --max_cpus '14' \
+    --max_memory '60GB' \
+    --outdir $PWD \
+    -resume
+
