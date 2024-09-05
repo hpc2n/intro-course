9947faaa33219546a9fd879d511a6e70fc9212e6
Pedro Ojeda-May <pedroojeda2011@gmail.com>
2024-09-05 06:31:16 +0200

    JUPYTERNB: usage

--- exercises/JUPYTERNOTEBOOKS/job.sh
+++ exercises/JUPYTERNOTEBOOKS/job.sh
@@ -6,12 +6,12 @@
 # Ask for a suitable amount of time. Remember, this is the time the Jupyter notebook will be available! HHH:MM:SS.
 #SBATCH --time=00:10:00
 # If you use the GPU nodes uncomment the following lines
-###SBATCH --gpus=v100:1
+###SBATCH --gpus=l40s:1
 
 # Clear the environment from any previously loaded modules
 module purge > /dev/null 2>&1
 # Load the module environment suitable for the job                                                                                       
-module load GCCcore/12.3.0 JupyterLab/4.0.5
+module load GCCcore/13.2.0 JupyterLab/4.2.0
 # Start JupyterLab
 jupyter lab --no-browser --ip $(hostname)
 
