af0e84888a4cc602f35826c568d66c85be763344
Pedro Ojeda-May <pedroojeda2011@gmail.com>
2024-09-05 10:50:45 +0200

    JUPYTERNB: usage

---
+++ exercises/JUPYTERNOTEBOOKS/INFOMAP/job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash          
+# Here you should put your own project id
+#SBATCH -A Project_ID
+# This example asks for 1 core
+#SBATCH -n 1         
+# Ask for a suitable amount of time. Remember, this is the time the Jupyter notebook will be available! HHH:MM:SS.
+#SBATCH --time=00:20:00
+# If you use the GPU nodes uncomment the following lines
+###SBATCH --gpus=l40s:1
+
+# Clear the environment from any previously loaded modules
+module purge > /dev/null 2>&1
+# Load the module environment suitable for the job                                                                                       
+module load GCCcore/13.2.0 JupyterLab/4.2.0
+
+# Source the environment
+source ./infmpenv/bin/activate
+# Start JupyterLab
+jupyter lab --no-browser --ip $(hostname)
+
