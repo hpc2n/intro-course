f24d0e6502632dc4b3998dc82146713386db2c82
Pedro Ojeda-May <pedroojeda2011@gmail.com>
2026-01-22 14:19:58 +0100

    NAMD: MPI jobs

--- exercises/NAMD/MPI/job-mpi.sh
+++ exercises/NAMD/MPI/job-mpi.sh
@@ -4,15 +4,13 @@
 #SBATCH -t 00:30:00
 #SBATCH -N 1
 #SBATCH -n 28
-#SBATCH -C intel_cpu
 
 #Load modules necessary for running NAMD
 ml purge  > /dev/null 2>&1 
-ml GCC/9.3.0  OpenMPI/4.0.3
-ml NAMD/2.14-mpi
+ml GCC/13.3.0  OpenMPI/5.0.3 NAMD/3.0.1-mpi 
 
 # Running NAMD 
-srun namd2 step4_equilibration.inp > output-mpi1.dat
+srun namd3 step4_equilibration.inp > output-mpi1.dat
 
 # Running NAMD with Multi Time Stepping algorithm 
-#srun namd2 step4_equilibration_mts.inp > output-mpi2.dat
+srun namd3 step4_equilibration_mts.inp > output-mpi2.dat
