095a37580eaa4eb6225ff9090f3bb97870eecdc3
Pedro Ojeda-May <pedroojeda2011@gmail.com>
2026-03-19 10:04:58 +0100

    NAMD: GPU jobs

--- exercises/NAMD/GPU/job-gpu.sh
+++ exercises/NAMD/GPU/job-gpu.sh
@@ -5,18 +5,14 @@
 #SBATCH -o output_%j.out   # output file
 #SBATCH -e error_%j.err    # error messages
 # Choose the GPU Type
-#SBATCH --gpus-per-node=Type:1
+#SBATCH --gpus-per-node=a100:1
 
 echo $CUDA_VISIBLE_DEVICES
 
 #Load modules necessary for running NAMD
 ml purge  > /dev/null 2>&1 
-ml GCC/11.3.0  OpenMPI/4.1.4
-ml NAMD/2.14-CUDA-11.7.0
+ml GCC/13.3.0  OpenMPI/5.0.3 NAMD/3.0.1-CUDA-12.6.0
 
-# GPU info
-nvidia-smi
-echo "nr. cps on node" 
-echo  $SLURM_CPUS_ON_NODE
-namd2 +p$SLURM_CPUS_ON_NODE +setcpuaffinity +idlepoll +devices $CUDA_VISIBLE_DEVICES step4_equilibration.inp > output-nvidia.dat
+namd3 +p1 +setcpuaffinity +idlepoll +devices $CUDA_VISIBLE_DEVICES step4_equilibration.inp > output-nvidia.dat
+
 
